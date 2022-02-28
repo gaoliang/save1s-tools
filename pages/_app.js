36c9aca0a3bfd8fe578b284b3052e90c92dc007e
gaoliang <gaoliangim@gmail.com>
2022-02-28 20:07:40 +0800

    fix build

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
-import Layout from '../src/layout'
+import Layout from '../src/Layout'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
